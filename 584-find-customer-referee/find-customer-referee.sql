85bd7c917b35815a1a157d6aff1cf31c28d93761
Jay Gupta <jaygupta4520@gmail.com>
2026-08-25 14:38:28 +0530

    Time: 532 ms (50.10%) | Memory: 0B (100.00%) - LeetSync

--- 584-find-customer-referee/find-customer-referee.sql
+++ 584-find-customer-referee/find-customer-referee.sql
@@ -1,2 +1,8 @@
 # Write your MySQL query statement below
-Select name from Customer where referee_id != 2 or referee_id is null
+Select name
+from Customer 
+where referee_id != 2 
+UNION ALL
+Select name
+from Customer 
+where referee_id is null
