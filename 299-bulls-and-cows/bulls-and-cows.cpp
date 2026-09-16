4e520d0507e48f46a3a4011837e442cf31f2f247
Jay Gupta <jaygupta4520@gmail.com>
2026-09-16 20:06:24 +0530

    Time: 2 ms (30.69%) | Memory: 9 MB (51.22%) - LeetSync

--- 299-bulls-and-cows/bulls-and-cows.cpp
+++ 299-bulls-and-cows/bulls-and-cows.cpp
@@ -1,32 +1,26 @@
 class Solution {
 public:
     string getHint(string secret, string guess) {
-
-        int aCount = 0;
-        int bCount = 0;
         int n = secret.size();
-
-        map<char,int> mp;
-
-        for(int i = 0; i < n; i++) {
-            if(secret[i] != guess[i]) {
+        map<int, int> mp;
+        for(int i=0; i<n; i++){
+            if(secret[i] != guess[i]){
                 mp[guess[i]]++;
             }
         }
+        int aCount = 0;
+        int bCount = 0;
 
-        for(int i = 0; i < n; i++) {
-
-            if(secret[i] == guess[i]) {
+        for(int i=0; i<n ; i++){
+            if(secret[i] == guess[i]){
                 aCount++;
-            }
-            else {
+            }else{
                 if(mp.count(secret[i]) && mp[secret[i]] > 0) {
                     bCount++;
                     mp[secret[i]]--;
                 }
             }
         }
-
-        return to_string(aCount) + 'A' + to_string(bCount) + 'B';
+        return to_string(aCount)+'A'+ to_string(bCount) +'B';
     }
 };
